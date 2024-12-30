c275c70d0704c95d562f875d458744155a76800f
Raj Randive <randiveraj96@gmail.com>
2024-12-30 14:58:55 +0530

    added vercel.json

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["http://localhost:5173", ""],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }
+));
 app.use(cookieParser());
 
 // Database connection
